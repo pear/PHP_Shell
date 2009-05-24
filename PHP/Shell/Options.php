3171d51e64f78506eac319bb55e6085f54853704
Daniel O'Connor <clockwerx@php.net>
2009-05-24 03:02:28 +0000

    Request #14938 Improve PHPCS results
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_Shell/trunk@281023 c90b9560-bf6c-de11-be94-00142212c4b1

--- PHP/Shell/Options.php
+++ PHP/Shell/Options.php
@@ -1,62 +1,82 @@
 <?php
-
 require_once "PHP/Shell/Extensions.php"; /* for the PHP_Shell_Interface */
   
 /**
-*
-*/
-class PHP_Shell_Options implements PHP_Shell_Extension {
-    /*
-    * instance of the current class
-    *
-    * @var PHP_Shell_Options
-    */
+ *
+ */
+class PHP_Shell_Options implements PHP_Shell_Extension
+{
+    /**
+     * instance of the current class
+     *
+     * @var PHP_Shell_Options
+     */
     static protected $instance;
 
     /**
-    * known options and their setors
-    *
-    * @var array
-    * @see registerOption
-    */
+     * known options and their setors
+     *
+     * @var array
+     * @see registerOption
+     */
     protected $options = array();
 
     /**
-    * known options and their setors
-    *
-    * @var array
-    * @see registerOptionAlias
-    */
+     * known options and their setors
+     *
+     * @var array
+     * @see registerOptionAlias
+     */
     protected $option_aliases = array();
  
-    public function register() {
+    /**
+     * Register
+     *
+     * @return void
+     */
+    public function register()
+    {
         $cmd = PHP_Shell_Commands::getInstance();
-        $cmd->registerCommand('#^:set #', $this, 'cmdSet', ':set <var>', 'set a shell variable');
+        $cmd->registerCommand('#^:set #', $this, 'cmdSet',
+                              ':set <var>', 'set a shell variable');
     }
 
     /**
-    * register a option
-    *
-    * @param string name of the option
-    * @param object a object handle
-    * @param string method-name of the setor in the object
-    * @param string (unused)
-    */ 
-    public function registerOption($option, $obj, $setor, $getor = null) {
+     * register a option
+     *
+     * @param string $option name of the option
+     * @param object $obj    a object handle
+     * @param string $setor  method-name of the setor in the object
+     * @param string $getor  (unused)
+     *
+     * @return void
+     */ 
+    public function registerOption($option, $obj, $setor, $getor = null)
+    {
         if (!method_exists($obj, $setor)) {
-            throw new Exception(sprintf("setor %s doesn't exist on class %s", $setor, get_class($obj)));
+            $msg = sprintf(
+                "setor %s doesn't exist on class %s",
+                $setor, get_class($obj)
+            );
+
+            throw new Exception($msg);
         }
 
         $this->options[trim($option)] = array("obj" => $obj, "setor" => $setor);
     }
 
     /**
-    * set a shell-var
-    *
-    * :set al to enable autoload
-    * :set bg=dark to enable highlighting with a dark backgroud
-    */
-    public function cmdSet($l) {
+     * set a shell-var
+     *
+     * :set al to enable autoload
+     * :set bg=dark to enable highlighting with a dark backgroud
+     *
+     * @param string $l Unknown
+     *
+     * @return void
+     */
+    public function cmdSet($l)
+    {
         if (!preg_match('#:set\s+([a-z]+)\s*(?:=\s*([a-z0-9]+)\s*)?$#i', $l, $a)) {
             print(':set failed: either :set <option> or :set <option> = <value>');
             return;
@@ -66,23 +86,27 @@ class PHP_Shell_Options implements PHP_Shell_Extension {
     }
 
     /**
-    * get all the option names
-    *
-    * @return array names of all options
-    */
-    public function getOptions() {
+     * get all the option names
+     *
+     * @return array names of all options
+     */
+    public function getOptions()
+    {
         return array_keys($this->options);
     }
 
     /**
-    * map a option to another option
-    *
-    * e.g.: bg maps to background
-    *
-    * @param string alias
-    * @param string option
-    */
-    public function registerOptionAlias($alias, $option) {
+     * map a option to another option
+     *
+     * e.g.: bg maps to background
+     *
+     * @param string $alias  Alias (name)
+     * @param string $option Option
+     *
+     * @return void
+     */
+    public function registerOptionAlias($alias, $option)
+    {
         if (!isset($this->options[$option])) {
             throw new Exception(sprintf("Option %s is not known", $option));
         }
@@ -92,13 +116,17 @@ class PHP_Shell_Options implements PHP_Shell_Extension {
     }
 
     /**
-    * execute a :set command
-    *
-    * calls the setor for the :set <option>
-    * 
-    * 
-    */
-    private function execute($key, $value) {
+     * execute a :set command
+     *
+     * calls the setor for the :set <option>
+     * 
+     * @param string $key   Key to set
+     * @param mixed  $value Value to write
+     *
+     * @return void
+     */
+    protected function execute($key, $value)
+    {
         /* did we hit a alias (bg for backgroud) ? */
         if (isset($this->option_aliases[$key])) {
             $opt_key = $this->option_aliases[$key];
@@ -120,7 +148,15 @@ class PHP_Shell_Options implements PHP_Shell_Extension {
         $obj->$setor($key, $value);
     }
 
-    static function getInstance() {
+    /**
+     * Return an instance of this
+     * 
+     * @todo Remove this! Singletons are bad
+     *
+     * @return PHP_Shell_Options
+     */
+    static function getInstance()
+    {
         if (is_null(self::$instance)) {
             $class = __CLASS__;
             self::$instance = new $class();
